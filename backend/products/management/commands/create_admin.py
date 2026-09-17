40ac7d728aee4961ec3598abca161a27720e0763
Brelle <moueleyembi@gmail.com>
2026-09-17 16:27:54 +0000

    Force correction username + mot de passe admin

--- backend/products/management/commands/create_admin.py
+++ backend/products/management/commands/create_admin.py
@@ -1,11 +1,21 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        user = User.objects.filter(username="Brelle").first()
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        user = User.objects.filter(email=email).first()
         if user:
-            user.username = "moueleyembi@gmail.com"
+            user.username = email
+            user.set_password(password)
+            user.is_staff = True
+            user.is_superuser = True
             user.save()
-            self.stdout.write("Username mis à jour")
+            self.stdout.write(self.style.SUCCESS(f"Utilisateur {email} corrigé (username + mot de passe)"))
+        else:
+            User.objects.create_superuser(username=email, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f"Superuser {email} créé"))
